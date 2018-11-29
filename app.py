c6e45134046fda0324a311eee7696402f16481ba
juanfpo96 <juanfpo96@gmail.com>
2018-11-29 16:28:22 +0100

    Finished landmarks

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
 from flask import Flask, abort, request
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return f'Error, recurso no encontrado: {error}', 404
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def default_entities():
+    return get_default_entities()
+
+
+@app.route('/LandmarksOrHistoricalBuildings')
+def all_landmarks():
+    return get_all_landmarks()
 
 
 @app.route('/LandmarksOrHistoricalBuildings/<int:entity_id>', methods=['GET', 'PUT', 'DELETE'])
 def landmarks(entity_id):
     if request.method == 'POST':
-        pass
+        create_landmark(request.get_json())
     elif request.method == 'GET':
         landmark = get_landmark(entity_id)
     elif request.method == 'PUT':
-        pass
+        update_landmark(entity_id, request.get_json())
     elif request.method == 'DELETE':
         landmark = get_landmark(entity_id)
         if not remove_landmark(entity_id):
             abort(401)
     else:
+        abort(401)
         return "Not valid method"
 
     if landmark.validate():
-        return landmark.to_json()
+        return send_response(landmark)
     else:
+        abort(401)
         return "Not valid"
 
 
@@ -38,20 +52,20 @@ def places(entity_id):
     elif request.method == 'GET':
         place = get_place(entity_id)
     elif request.method == 'PUT':
-        pass
+        update_place(entity_id, request.get_json())
     elif request.method == 'DELETE':
         pass
     else:
         return "Not valid method"
 
     if place.validate():
-        return place.to_json()
+        return send_response(place)
     else:
         return "Not valid"
 
 
 def get_landmark(entity_id):
-    f = open(f'./landmarks/{entity_id}.json', 'r')
+    f = open(f'./landmarks/{entity_id}.json', 'r', encoding='utf-8')
     obj = json.load(f)
     return Landmark(obj)
 
@@ -65,10 +79,62 @@ def remove_landmark(entity_id):
         return False
 
 
+def update_landmark(entity_id, json_file):
+    f = open(f'./landmarks/{entity_id}.json', 'w', encoding='utf-8')
+    landmark = Landmark(json_file)
+    if landmark.validate():
+        json.dump(landmark, f, ensure_ascii=False)
+    return landmark.to_json()
+
+
+def get_all_landmarks():
+    landmarks_dir = os.listdir('./landmarks/')
+    landmarks_arr = []
+    for file in landmarks_dir:
+        entity_id = os.path.splitext(file)[0]
+        landmarks_arr.append(get_landmark(entity_id).to_json())
+    print(json.dumps(landmarks_arr))
+
+
+def create_landmark(json_file):
+    landmarks_dir = os.listdir('./landmarks/')
+    last_file = os.path.splitext(landmarks_dir[0])[0]
+    entity_id = last_file + 1
+    landmark = Landmark(json_file)
+    if landmark.validate():
+        f = open(f'./landmarks/{entity_id}.json', 'w', encoding='utf-8')
+        json.dump(landmark, f, ensure_ascii=False)
+    return landmark.to_json()
+
+
+# Places
 def get_place(entity_id):
-    f = open(f'./places/{entity_id}.json', 'r')
+    f = open(f'./places/{entity_id}.json', 'r', encoding='utf-8')
     obj = json.load(f)
     return Place(obj)
+
+
+def update_place(entity_id, json_file):
+    f = open(f'./landmarks/{entity_id}.json', 'w', encoding='utf-8')
+    place = Place(json_file)
+    if place.validate():
+        json.dump(place, f, ensure_ascii=False)
+    return place.to_json()
+
+
+# Common functions
+def send_response(response):
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    if best == 'application/json':
+        return response.to_json()
+    else:
+        return response.to_html()
+
+
+def get_default_entities():
+    f = open('./entities/entities.json', 'r', encoding='utf-8')
+    return json.dumps(json.load(f))
 
 
 class Landmark:
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Python Flask Server
+
+* Added Flask-cors extension
+* Read/write JSON
+* Manual parse JSON to object
+* Deployed at: 
